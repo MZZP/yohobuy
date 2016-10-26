61f4ca778533e9afd60b4fd3eb3e054985297658
ZHzxiansheng <15738776716@163.com>
2016-10-26 21:08:59 +0800

    '壮2'

---
+++ myorder/js/myorder.js
@@ -0,0 +1,4 @@
+// var docheight = document.body.offsetHeight;
+var docheight = window.screen.height;
+// console.log(docheight);
+document.getElementById('myorder').style.height = docheight + 'px';
